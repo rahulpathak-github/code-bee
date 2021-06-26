54bb9bd928a7d8da3b9ce317a093ed8e1d3c46ca
rahulpathak-github <62535734+rahulpathak-github@users.noreply.github.com>
2021-06-26 17:29:50 +0530

    Add comment routes, update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,22 +1,5 @@
 /* eslint-disable max-len */
-/* !
 
-=========================================================
-* Argon React NodeJS - v1.0.0
-=========================================================
-
-* Product Page: https://argon-dashboard-react-nodejs.creative-tim.com/
-* Copyright 2020 Creative Tim (https://https://www.creative-tim.com//)
-* Copyright 2020 ProjectData (https://projectdata.dev/)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react-nodejs/blob/main/README.md)
-
-* Coded by Creative Tim & ProjectData
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -30,6 +13,7 @@ const path = require('path');
 const db = require('./config/keys').mongoURI;
 const CronJob = require('cron').CronJob;
 const crons = require('./config/crons');
+const comments = require("./Comments/comments");
 
 require('dotenv').config();
 
@@ -44,22 +28,22 @@ require('./config/passport')(passport);
 
 // Connect to MongoDB
 mongoose
-    .connect(
-        db, {useNewUrlParser: true,
-          useFindAndModify: false,
-          useUnifiedTopology: true,
-          useCreateIndex: true},
-    )
-    .then(() => console.log('MongoDB Connected'))
-    .catch((err) => console.log(err));
+  .connect(
+    db, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  },
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 
-
-
 // Express body parser
 app.use('/public', express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
@@ -74,9 +58,11 @@ if (process.env.NODE_ENV === 'PROD') {
 
 // Initialize routes middleware
 app.use('/api/users', require('./routes/users'));
+app.use('/api/course', require('./routes/course'));
+app.use('/api/comment', require('./routes/comment'));
 
 // run at 3:10 AM -> delete old tokens
-const tokensCleanUp = new CronJob('10 3 * * *', function() {
+const tokensCleanUp = new CronJob('10 3 * * *', function () {
   crons.tokensCleanUp();
 });
 tokensCleanUp.start();
@@ -84,14 +70,17 @@ tokensCleanUp.start();
 const PORT = process.env.PORT;
 
 
-http.createServer({
-}, app)
-    .listen(PORT, function() {
-      console.log('App listening on port ' + PORT + '! Go to http://localhost:' + PORT + '/');
-    });
+// http.createServer({
+// }, app)
+//   .listen(PORT, function () {
+//     console.log('App listening on port ' + PORT + '! Go to http://localhost:' + PORT + '/');
+//   });
 
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT} ...`);
+});
 
-
+comments.setupSocket(server);
 // FOR HTTPS ONLY
 // https.createServer({
 //   key: fs.readFileSync(process.env.SSLKEY),
