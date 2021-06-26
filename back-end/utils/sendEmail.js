8590ee18536f823108598c1341293f4b8bda2eb7
rivatus <rivatus3@gmail.com>
2021-06-26 15:43:00 +0530

    Add nodemailer, course Controller and routes

--- back-end/utils/sendEmail.js
+++ back-end/utils/sendEmail.js
@@ -1,34 +1,24 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-    let transport;
-    if (process.env.NODE_ENV === "production") {
-        transport = nodemailer.createTransport({
-            service: "SendGrid",
-            auth: {
-                user: process.env.SENDGRID_USERNAME,
-                pass: process.env.SENDGRID_PASSWORD,
-            },
-        });
-    } else {
-        transport = nodemailer.createTransport({
-            host: 'smtp.googlemail.com',
-            port: 465,
-            auth: {
-                user: process.env.EMAIL_ID,
-                pass: process.env.EMAIL_PASSWORD,
-            },
-        });
-    }
-    const mailOptions = {
-        from: "<rahulmpathak1@gmail.com>",
+    let transport = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'lqmqba@gmail.com',
+            pass: 'nodeMailerTest',
+        },
+    });
+
+    let mailOptions = {
+        from: "lqmqba@gmail.com",
         to: options.email,
         subject: options.subject,
-        //text: options.message,
         html: options.html,
     };
 
-    await transport.sendMail(mailOptions, (error, info) => {
+    transport.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.log(error);
         } else {
