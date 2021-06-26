d9b7d7b42cbd1e522b909073425ecfdf3a1d529a
rahulpathak-github <62535734+rahulpathak-github@users.noreply.github.com>
2021-06-26 15:41:20 +0530

    commit userController again

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -6,6 +6,7 @@ const User = require('../models/user');
 const ActiveSession = require('../models/activeSession');
 //const { smtpConf } = require('../config/config');
 const sendEmail = require('../utils/sendEmail')
+const crypto = require("crypto");
 
 exports.getProfile = async (req, res, next) => {
     try {
@@ -80,18 +81,17 @@ exports.register = async (req, res, next) => {
                     User.create(query, async (err, user) => {
                         if (err) throw err;
 
-
                         if (process.env.DEMO != 'yes') {
 
                             await sendEmail({
                                 email: email,
                                 subject: "Confirm your account",
-                                html: '<h1>Hey,</h1><br><p>Confirm your new account </p><p><a href="' + 'http://localhost:3000/auth/confirm-email/' + user._id + '">"' + 'http://localhost:3000/auth/confirm-email/' + user._id + '"</a></p>'
+                                html: '<h1>Hey,</h1><br><p>Confirm your new account </p><p><a href="' + 'http://localhost:5100/api/users/confirm/' + user._id + '">"' + 'verify!' + '"</a></p>'
                             })
 
                             res.json({ success: true, msg: 'The user was succesfully registered' });
                         }
-
+                        // eslint-disable-next-line max-len
                         else res.json({ success: true, userID: user._id, msg: 'The user was succesfully registered' });
                     });
                 });
@@ -114,4 +114,77 @@ exports.logout = async (req, res, next) => {
         //throw err
     }
 
+}
+
+exports.confirmMail = async (req, res, next) => {
+    try {
+        const userId = req.params.id
+        const user = await User.findOne({ _id: userId })
+        if (!user) return res.json({ success: true });
+        const query = { _id: userId };
+
+        const newvalues = { $set: { verified: true } };
+        await User.updateOne(query, newvalues)
+        res.json({ success: true });
+
+    } catch (err) {
+        throw err
+    }
+}
+
+exports.forgotPassword = async (req, res, next) => {
+    const { email } = req.body
+    const user = await User.findOne({
+        email: email,
+    });
+    if (!user) return res.json({ success: false, msg: "Email not registered!" })
+    //console.log(user);
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+    const html = `<p>Forgot your password? <br> Paste this Code on your screen and enter your New Password.<br>  If you didn't forget your password, please ignore this email!<br>   ${resetToken} <br> </p> `;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: "Your Password Reset Token {Valid for 10 min}",
+            html: html,
+        });
+
+        res.json({
+            success: true,
+            msg: "Token sent to mail",
+        });
+    } catch (err) {
+        user.resetPassToken = undefined;
+        user.resetPassTokenExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+
+        throw err;
+    }
+}
+
+exports.resetPassword = async (req, res, next) => {
+    try {
+        const resetToken = crypto
+            .createHash("sha256")
+            .update(req.body.token)
+            .digest("hex");
+
+        const user = await User.findOne({
+            resetPassToken: resetToken,
+            resetPassTokenExpires: { $gt: Date.now() },
+        });
+
+        if (!user) {
+            return res.json({ success: false, msg: "Invalid user" });
+        }
+
+        user.password = req.body.password;
+        user.resetPassToken = undefined;
+        user.resetPassTokenExpires = undefined;
+        await user.save();
+        res.json({ success: true, msg: "Password reset successfully" })
+    } catch (err) {
+        throw err;
+    }
 }
