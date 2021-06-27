4fd640208f6b6473be7f918022ad5807d4ed76d4
rahulpathak-github <62535734+rahulpathak-github@users.noreply.github.com>
2021-06-27 14:50:57 +0530

    Changed footer contents

--- front-end/src/components/Footers/AuthFooter.js
+++ front-end/src/components/Footers/AuthFooter.js
@@ -30,13 +30,13 @@ class Login extends React.Component {
             <Row className="align-items-center justify-content-xl-between">
               <Col xl="6">
                 <div className="copyright text-center text-xl-left text-muted">
-                  © 2018{" "}
+                  © 2021{" "}
                   <a
                     className="font-weight-bold ml-1"
-                    href="https://www.creative-tim.com?ref=adr-auth-footer"
+                    //href="https://www.creative-tim.com?ref=adr-auth-footer"
                     target="_blank"
                   >
-                    Creative Tim
+                    Codebee
                   </a>
                 </div>
               </Col>
@@ -44,15 +44,15 @@ class Login extends React.Component {
                 <Nav className="nav-footer justify-content-center justify-content-xl-end">
                   <NavItem>
                     <NavLink
-                      href="https://www.creative-tim.com?ref=adr-auth-footer"
+                      // href="https://www.creative-tim.com?ref=adr-auth-footer"
                       target="_blank"
                     >
-                      Creative Tim
+                      Codebee
                     </NavLink>
                   </NavItem>
                   <NavItem>
                     <NavLink
-                      href="https://www.creative-tim.com/presentation?ref=adr-auth-footer"
+                      // href="https://www.creative-tim.com/presentation?ref=adr-auth-footer"
                       target="_blank"
                     >
                       About Us
@@ -60,20 +60,20 @@ class Login extends React.Component {
                   </NavItem>
                   <NavItem>
                     <NavLink
-                      href="http://blog.creative-tim.com?ref=adr-auth-footer"
+                      // href="http://blog.creative-tim.com?ref=adr-auth-footer"
                       target="_blank"
                     >
                       Blog
                     </NavLink>
                   </NavItem>
-                  <NavItem>
+                  {/* <NavItem>
                     <NavLink
-                      href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-auth-footer"
+                     // href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-auth-footer"
                       target="_blank"
                     >
                       MIT License
                     </NavLink>
-                  </NavItem>
+                  </NavItem> */}
                 </Nav>
               </Col>
             </Row>
